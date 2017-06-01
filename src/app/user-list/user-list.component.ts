b53d4502f578f678f648b11e4ca29ccfb7d88cf7
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-06-01 12:48:09 +0300

    Created array Created a local names array variable defined the array in the constructor with names

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  names:string[]
+
+  constructor() {
+    this.names=["James","Popo","Nombu","Kahu"];
+   }
 
   ngOnInit() {
   }
